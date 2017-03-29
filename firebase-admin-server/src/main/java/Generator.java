758c484e9a747d28eb8ad59c915d6b453de637c7
Lapiki <alapik13@student.aau.dk>
2017-03-29 13:07:20 +0200

    Comments

--- firebase-admin-server/src/main/java/Generator.java
+++ firebase-admin-server/src/main/java/Generator.java
@@ -1,21 +1,34 @@
 import java.util.Random;
 
 /**
- * Created by lapiki on 3/14/17.
+ * A class that can generate a Minion object.
  */
 public class Generator {
     Random rand = new Random();
 
 
+    /**
+     * Generates a Minion object.
+     * @return The generated Minion object.
+     */
     public Minion generateMinion(){
         int level = rand.nextInt(50) + 1;
-        double lon = (rand.nextDouble() * 2) + 56;
-        double lat = (rand.nextDouble() * 3) + 8;
 
-        return generateMinionOfTypeType(level, lon, lat);
+        //Coordinates that are restricted to north jutland.
+        double lon = (rand.nextDouble()) + 55.5;
+        double lat = (rand.nextDouble()) + 9.5;
+
+        return generateMinionOfType(level, lon, lat);
     }
 
-    public Minion generateMinionOfTypeType(int level, double lon, double lat) {
+    /**
+     * Generates a Minion object of a rondom type.
+     * @param level The level og the Minion.
+     * @param lon Longitude of the Minion.
+     * @param lat Latitude of the Minion.
+     * @return Returns the Minion Object.
+     */
+    public Minion generateMinionOfType(int level, double lon, double lat) {
 
         int num = rand.nextInt(2);
         Minion m = null;

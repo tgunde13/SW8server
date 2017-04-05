adac492acdacd84c840aeb85a21999879880da2f
Lapiki <alapik13@student.aau.dk>
2017-04-05 12:29:53 +0200

    Test test test

--- firebase-admin-server/src/test/java/ServerTest.java
+++ firebase-admin-server/src/test/java/ServerTest.java
@@ -1,7 +1,19 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by lapiki on 4/3/17.
  */
 public class ServerTest {
+
+    @Test
+    public void queueMinionCreationTest() {
+        QueueMinion q = new QueueMinion("aaa", 5 , 1000);
+
+        assertTrue("aaa".equals(q.key));
+        assertEquals(5, q.zone);
+        assertEquals(1000, q.timestamp);
+    }
 }

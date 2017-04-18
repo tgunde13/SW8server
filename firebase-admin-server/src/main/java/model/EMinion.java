c6c9bc8b6d6851cb7572b255a2a1170e055c605b
Archimedez <covese13@student.aau.dk>
2017-04-18 15:18:07 +0200

    changes

--- firebase-admin-server/src/main/java/model/EMinion.java
+++ firebase-admin-server/src/main/java/model/EMinion.java
@@ -16,7 +16,7 @@ public class EMinion extends Minion {
      * @param lat the latitude of the minion
      * @param lon the longitude of the minion
      */
-    EMinion(double lat, double lon) {
+    public EMinion(double lat, double lon) {
         this.lat = lat;
         this.lon = lon;
 

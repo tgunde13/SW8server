07b8c425f35d4f230b9946e102867dcdf2374d99
tgunde13 <tgunde13@student.aau.dk>
2017-05-09 09:57:29 +0200

    test af ai moves

--- firebase-admin-server/src/test/java/battle/EnvironmentMovesHandlerTest.java
+++ firebase-admin-server/src/test/java/battle/EnvironmentMovesHandlerTest.java
@@ -16,7 +16,7 @@ import static org.junit.Assert.*;
  */
 public class EnvironmentMovesHandlerTest {
     @Test
-    public void addMoves() throws Exception {
+    public void addMoves1Opponent() throws Exception {
         // Player team
         final Map<String, BattleAvatar> playerTeam = new HashMap<>();
         final BattleAvatar playerAvatar = new BattleAvatar();
@@ -36,6 +36,37 @@ public class EnvironmentMovesHandlerTest {
         EnvironmentMovesHandler.addMoves(state, choices);
 
         assertEquals(1, choices.getMoves().size());
+        assertEquals(3, choices.getMoves().get("eKey1").size());
+
+        for (ChosenMove move : choices.getMoves().get("eKey1").values()) {
+            assertEquals("uid1", move.getAvatarKey());
+            assertEquals("pMinion1", move.getMinionKey());
+        }
+    }
+
+    @Test
+    public void addMoves2Opponents() throws Exception {
+        // Player team
+        final Map<String, BattleAvatar> playerTeam = new HashMap<>();
+        final BattleAvatar playerAvatar = new BattleAvatar();
+        playerAvatar.addMinion("pMinion1", new PlayerMinion("name1", 1, 6, 1, 1, "Melee"));
+        playerAvatar.addMinion("pMinion2", new PlayerMinion("name2", 1, 6, 1, 1, "Melee"));
+        playerTeam.put("uid1", playerAvatar);
+
+        // E team
+        final Map<String, BattleAvatar> eTeam = new HashMap<>();
+        eTeam.put("eKey1", new BattleAvatar(new EMinionTemplate(57.0, 10.0, 3, "name", 2, 5, 1, 1, "Melee")));
+
+        final BattleState state = new BattleState(playerTeam, eTeam);
+
+        final AvatarChoices choices = new AvatarChoices(new HashMap<>());
+
+        assertEquals(0, choices.getMoves().size());
+
+        EnvironmentMovesHandler.addMoves(state, choices);
+
+        assertEquals(1, choices.getMoves().size());
+        assertEquals(3, choices.getMoves().get("eKey1").size());
     }
 
 }
